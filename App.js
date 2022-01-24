255df56c523566ca464e9ff48bc25ed674ed6ce7
Nishan <nishanbende@gmail.com>
2022-01-24 15:48:58 +0530

    init

--- App.js
+++ App.js
@@ -1,20 +1,90 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button ,FlatList, ScrollView} from "react-native";
+import { Video } from "expo-av";
+
+const data = [
+  "http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4",
+  "http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4",
+  "http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4",
+  "http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4",
+  "http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4",
+  "http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4",
+  "http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4",
+  "http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4",
+  "http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4",
+  "http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4",
+  "http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4",
+  "http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4",
+  "http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4",
+  "http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4",
+  "http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4",
+  "http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4",
+  "http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4",
+  "http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4",
+  "http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4",
+  "http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4",
+  "http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4",
+  "http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4",
+  "http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4",
+  "http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4",
+  "http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4",
+  "http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4",
+  "http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4",
+  "http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4",
+  "http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4",
+  "http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4",
+  "http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4",
+  "http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4",
+  "http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4",
+  "http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4",
+  "http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4",
+  "http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4",
+  "http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4",
+  "http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4",
+];
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <FlatList
+        data={data}
+        keyExtractor={(item, idx) => idx.toString()}
+        renderItem={renderItem}
+        removeClippedSubviews
+        windowSize={1}
+        numColumns={1}
+        numColumns={3}
+      />
+      {/* <ScrollView>
+        {data.map((item, idx) => {
+          return <View key={idx}>{renderItem({item})}</View>
+        })}
+      </ScrollView> */}
     </View>
   );
+}
+
+const renderItem = ({item}) => {
+   return (
+     <Video
+       style={{ width: 100, height: 100 }}
+       source={{
+         uri: item
+       }}
+       useNativeControls
+       resizeMode="cover"
+       isLooping
+       isMuted
+     />
+   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
